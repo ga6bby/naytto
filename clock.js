3061ff45b05832e93aac3a73e7b7c09104991088
ga6bby <gabbyw303@gmail.com>
2025-09-05 12:23:17 +0300

    add html, js

--- clock.js
+++ clock.js
@@ -3,20 +3,17 @@ function updateDateTime() {
     var day = now.getDate();
     var month = now.getMonth() + 1;
     var year = now.getFullYear();
+    if (day < 10) day = '0' + day;
+    if (month < 10) month = '0' + month;
+    document.getElementById("date").innerText = day + '.' + month + '.' + year;
+
     var hours = now.getHours();
     var minutes = now.getMinutes();
     var seconds = now.getSeconds();
-
+    if (hours < 10) hours = '0' + hours;
     if (minutes < 10) minutes = '0' + minutes;
     if (seconds < 10) seconds = '0' + seconds;
-
-    var datetimeString = 'Päivämäärä: ' + day + '.' + month + '.' + year + 
-                         ' | Kellonaika: ' + hours + ':' + minutes + ':' + seconds;
-
-    var datetimeElement = document.getElementById("current-datetime");
-    if (datetimeElement) {
-        datetimeElement.innerText = datetimeString;
-    }
+    document.getElementById("clock").innerText = hours + ':' + minutes + ':' + seconds;
 }
 
 updateDateTime();
